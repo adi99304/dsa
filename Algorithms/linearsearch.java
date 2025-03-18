5f7504b9ba1cba7b5ba1ead22c92dfd47c61cb49
adi99304 <adiaryanbrothers7@gmail.com>
2025-03-18 22:01:23 +0530

    linearsearch

--- Algorithms/linearsearch.java
+++ Algorithms/linearsearch.java
@@ -4,15 +4,17 @@ public class linearsearch {
     public static void main(String[] args) {
 int[] arr={2,3,4,5,6,7};
 int target=6;
-int a=linear(arr,target);
+int start=2;
+int end=4;
+int a=linear(arr,target,start,end);
         System.out.println(a);
 
     }
- static int linear(int[] arr,int target) {
+ static int linear(int[] arr,int target,int start,int end) {
      if (target == arr[0]) {
          System.out.println("we found the number at zero index");
      } else {
-         for (int i = 1; i < arr.length; i++) {
+         for (int i = start; i < end; i++) {
              if (target == arr[i]) {
                  return i;
              }
